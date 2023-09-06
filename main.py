55134e90d153d5977af77255de73bfded194d29e
mykyta <drobotda598@gmail.com>
2023-09-06 22:13:48 +0300

    done the 2 exercise

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import requests
-from flask import Flask
+from flask import Flask, request, Response
+from faker import Faker
 
+fake = Faker()
 app = Flask(__name__)
+
+
+# 2
+@app.get("/generate-users/")
+def generate():
+    query = request.args.get('num_users', default=100, type=int)
+    if query > 0:
+        list_users = []
+        for i in range(query):
+            list_users.append(f'{fake.name()}, {fake.email()}')
+        info = '\n'.join(list_users)
+        res = Response(info, content_type='text/plain')
+        return res
+    else:
+        return 'num_users must be greater than 0'
 
 
 # 4
