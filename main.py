5fb36b1e2aab531b038f72533e172ebcc61a22e3
mykyta <drobotda598@gmail.com>
2023-09-06 23:11:37 +0300

    done the 3 exercise

--- main.py
+++ main.py
@@ -21,6 +21,21 @@ def generate():
         return 'num_users must be greater than 0'
 
 
+# 3
+@app.get('/mean/')
+def mean():
+    file = open('hw.csv', 'r', encoding='utf-8')
+    info = file.readlines()
+    list_res = []
+    for el in info[1:]:
+        elem = el.strip('\n').split(',')
+        if not len(elem) < 3:
+            list_res.append(f'Index - {elem[0]}, height - {round(float(elem[1]) * 2.54, 2)},'
+                            f' weight - {round(float(elem[2]) * 0.454, 2)}')
+    res = Response('\n'.join(list_res), content_type='text/plain')
+    return res
+
+
 # 4
 @app.get("/space/")
 def count_space():
